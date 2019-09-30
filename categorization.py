d17f3876b31a0c331355a64ec579bf15c3c2a96f
Eduardo Calandrini <eduardocalandrini@hotmail.com>
2019-09-30 14:50:47 -0300

    categorizacao por palavras

--- categorization.py
+++ categorization.py
@@ -16,6 +16,7 @@ Created on Mon Sep  9 13:46:57 2019
 import pandas as pd
 #from tf_idf_pt import get_stop_words
 
+import json
 import unicodedata
 import re
 from collections import Counter
@@ -27,6 +28,9 @@ from Stopwords import stopwords as sw
 
 nltk.download('punkt')
 
+def getchar():
+    a = input('').split(" ")[0]
+    print(a)
 
 def remove_accents(input_str):
     nfkd_form = unicodedata.normalize('NFKD', input_str)
@@ -52,7 +56,7 @@ if __name__ == '__main__':
     
     stopwords = sw.get_stop_words()     # Carrega as stop-words
     
-    df = pd.read_csv('siconv_cgdad_v2.0_instrumentos.txt', sep="\t")
+    df = pd.read_csv('siconv_cgdad_v2.0_instrumentos_sample_100.txt', sep="\t")
     df_descricao = df['descricao_proposta']
 
     descricao_tokens = [[] for _ in range(len(df_descricao))]
@@ -60,7 +64,54 @@ if __name__ == '__main__':
     #tolenika cada comentario
     descricao_tokens = pre_process(df_descricao)
 
+    # TODO json to dictionary
+    with open('Clusters/base.json', 'r') as f:
+        clusters = json.load(f)
 
+    results = dict()
+    categorias_name = []
+
+    # TODO para cada linha saber quantas palavras da categoria especifica bate, ou seja,
+    # iterar para cada palavra em todas as categorias e somar 1 nas posições das categorias.
+    # Na proxima palvra da mesma linha, ao iterar, somar 1 à mesma posição 
+
+    for i,row in enumerate(descricao_tokens,start=0): # iteração por linha]
+        results[i] = dict()
+        for word in row: # iteração por palavra na linha
+            for cat_obj in clusters: # categorias gerais
+                for cat_arr_str in cat_obj:
+                    cat_arr = cat_obj[cat_arr_str]
+
+                    for cat2_obj in cat_arr: # sub categorias
+                        for cat2_arr_str in cat2_obj:
+                            cat2_arr = cat2_obj[cat2_arr_str]
+
+                            for cat3_obj in cat2_arr: # categoria especifica
+                                for cat3_arr_str in cat3_obj:
+                                    cat3_arr = cat3_obj[cat3_arr_str]
+
+                                    for cat4_obj in cat3_arr: # palavras da categoria
+                                        for cat4_arr_str in cat4_obj:
+                                            cat4_arr = cat4_obj[cat4_arr_str]
+
+                                            catname = cat_arr_str + '->' + cat2_arr_str
+                                            if cat3_arr_str != 'value':
+                                                catname += '->' + cat3_arr_str
+                                            if cat4_arr_str != 'value':
+                                                catname += '->' + cat4_arr_str
+
+                                            if catname not in categorias_name:
+                                                categorias_name.append(catname)
+
+                                            if catname not in results[i]:
+                                                results[i][catname] = 0
+
+                                            if word in cat4_arr:
+                                                results[i][catname] += 1
+                                            
+jsonO = json.dumps(results)
+print(jsonO)
+'''
  ####################################################
     #Exemplo : linha 161 do csvsiconv_cgdad_v2.0_instrumentos.txt
     print('Texto original:', df_descricao[161])
@@ -78,7 +129,7 @@ if __name__ == '__main__':
     cluster_Edu_Obras_Ref_QE = ['reforma', 'quadra', 'poliesportiva', 'esporte']
     cluster_Edu_Obras_Mod_QE = ['modernizacao', 'quadra', 'poliesportiva', 'esporte']
     
-    for w in descricao_tokens[161]: 
+    for w in descricao_tokens[161]:
         if w in cluster_Edu_Obras_Cons_QE:
             CM_Edu_Obras_Cons_QE =  CM_Edu_Obras_Cons_QE + 1
             
@@ -95,6 +146,4 @@ if __name__ == '__main__':
     print('Dicionario:',dictionary_edu_obras)
     
     print('Categoria:', max(dictionary_edu_obras, key=dictionary_edu_obras.get))
-    
-    
-    
+'''
